b0d5eaf752291857de5014c2fe6f4a7421a508dd
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-01-22 21:46:16 +0800

    fix: Fix Solid package Panda CSS config

--- packages/solid/panda.config.ts
+++ packages/solid/panda.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@pandacss/dev';
 export default defineConfig({
   preflight: true,
   include: ['./src/**/*.{js,jsx,ts,tsx}', './stories/**/*.{js,jsx,ts,tsx}'],
-  outdir: '../styled-system/dist',
+  outdir: './styled-system',
   jsxFramework: 'solid',
   presets: ['@hanekokoro-ui/panda-preset'],
   strictTokens: true,
