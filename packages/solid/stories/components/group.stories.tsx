36df21c594c62204f2ad9a000b6948ba8912aec1
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-03-11 14:53:34 +0800

    chore: Remove new from Group component

--- packages/solid/stories/components/group.stories.tsx
+++ packages/solid/stories/components/group.stories.tsx
@@ -11,7 +11,7 @@ import LinkDuotone from '~icons/ph/link-duotone';
 const meta: Meta<typeof Group> = {
   component: Group,
   title: 'Group',
-  tags: ['autodocs', 'new'],
+  tags: ['autodocs'],
   args: {
     attached: false,
     grow: false,
